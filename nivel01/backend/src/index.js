9471f96962f2ceeb14ff7b980849b4da33b55650
Michelle Romão <michelle.nunes10@gmail.com>
2020-04-09 13:57:44 -0300

    nivel 01, backend

--- nivel01/backend/src/index.js
+++ nivel01/backend/src/index.js
@@ -29,7 +29,7 @@ app.use("/projects/:id", validateProjectId);
 app.get("/projects", (request, response) => {
   const { title } = request.query;
   const results = title
-    ? projects.filter(project => project.title.includes(title))
+    ? projects.filter((project) => project.title.includes(title))
     : projects;
   return response.json(results);
 });
@@ -45,14 +45,14 @@ app.put("/projects/:id", (request, response) => {
   const { id } = request.params;
   const { title, owner } = request.body;
 
-  const projectIndex = projects.findIndex(project => project.id === id);
+  const projectIndex = projects.findIndex((project) => project.id === id);
   if (projectIndex < 0) {
     return response.status(400).json({ error: "Project Not Found" });
   }
   const project = {
     id,
     title,
-    owner
+    owner,
   };
 
   projects[projectIndex] = project;
@@ -62,7 +62,7 @@ app.put("/projects/:id", (request, response) => {
 
 app.delete("/projects/:id", (request, response) => {
   const { id } = request.params;
-  const projectIndex = projects.findIndex(project => project.id === id);
+  const projectIndex = projects.findIndex((project) => project.id === id);
   if (projectIndex < 0) {
     return response.status(400).json({ error: "Project Not Found" });
   }
@@ -72,6 +72,6 @@ app.delete("/projects/:id", (request, response) => {
   return response.status(204).send();
 });
 
-app.listen(3334, () => {
+app.listen(3333, () => {
   console.log("⭐  Backend started! ");
 });
